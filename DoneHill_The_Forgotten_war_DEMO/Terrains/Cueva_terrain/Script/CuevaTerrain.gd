3bb6b7579768cee0ad52307c125329a80526941e
GustavoCruz <gustavocq.dam@gmail.com>
2024-06-16 15:41:14 +0200

    fixing connections

---
+++ DoneHill_The_Forgotten_war_DEMO/Terrains/Cueva_terrain/Script/CuevaTerrain.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@onready var player = $Player
+
+func _ready():
+	GLOBAL.load_game()
+	
+	if NavigationManager.spawn_door_tag != null:
+		_on_level_spawn(NavigationManager.spawn_door_tag)
+		GLOBAL.Reload_Player_Settings(player, false)
+	else:
+		GLOBAL.Reload_Player_Settings(player, true)
+
+
+func _on_level_spawn(destination_tag: String):
+	var door_path = "Doors/Door_" + destination_tag
+	var door = get_node(door_path) as Door
+	NavigationManager.Trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
